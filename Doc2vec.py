10293309caa91f3fe62c94eb6b16715faf02432f
macjiong <760783896@qq.com>
2018-08-08 12:28:39 +0100

    adding doc2vec training code, classification algorithm code

--- Doc2vec.py
+++ Doc2vec.py
@@ -1,11 +1,12 @@
 from gensim.models import KeyedVectors
-import csv
+import csv,alg_bayes,alg_decision_tree,alg_logestic_regression,alg_ramdom_forest,alg_svm
+import pandas as pd
 # gensim modules
 from gensim import utils
 from gensim.models.doc2vec import LabeledSentence
 from gensim.models import Doc2Vec
 # numpy
-import numpy,linkedindata,datafilter,globalparameter, random,labeled_data_sentence
+import numpy,linkedindata,datafilter,globalparameter, random,labeled_data_sentence,linkedindata_old
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
@@ -19,85 +20,108 @@ from sklearn.linear_model import LogisticRegression
 # csvfile = csv.reader(open('/Users/pengyuzhou/Google Drive/Linkedin_datafile/data.csv',"r"))
 
 linkedIndata_list = []
-with open('/Users/pengyuzhou/Google Drive/Linkedin_datafile/data_v3.csv', 'r') as f:
+with open('/Users/pengyuzhou/Google Drive/Linkedin_datafile/data.csv', 'r') as f:
     next(f)
     reader = csv.reader(f)
     for row in reader:
-        parameter_list = [row[index] for index in range(112)]
+        parameter_list = [row[index] for index in range(67)]
         linkedIndata_list.append(
-            linkedindata.LinkedInData(*parameter_list))
+            linkedindata_old.LinkedInData(*parameter_list))
 
 for i in range(6):
-    average_precision = 0.0
-    average_recall = 0.0
-    average_accuracy = 0.0
     print('Began to read job title: '+ globalparameter.jobtitle_list[i])
-relevant_user_list = datafilter.filter_data_with_job_title_oo(linkedIndata_list, globalparameter.jobtitle_list[0],
-                                                              1)
-non_relevant_user_list = datafilter.filter_data_with_job_title_oo(linkedIndata_list,
-                                                                  globalparameter.jobtitle_list[0], 2)
-pos_list = random.sample(relevant_user_list, 500)
-neg_list = random.sample(non_relevant_user_list, 500)
+    relevant_user_list = datafilter.filter_data_with_job_title_oo(linkedIndata_list, globalparameter.jobtitle_list[i],
+                                                                  1)
+    non_relevant_user_list = datafilter.filter_data_with_job_title_oo(linkedIndata_list,
+                                                                      globalparameter.jobtitle_list[i], 2)
+    for j in range(5):
+        print('began the iteration: '+str(j+1)+' for job title: '+globalparameter.jobtitle_list[i])
+        pos_list = random.sample(relevant_user_list, 500)
+        neg_list = random.sample(non_relevant_user_list, 500)
 
+        # You can change the features you want to use
+        pos_profile_list = []
+        neg_profile_list = []
+        for k in range(len(pos_list)):
+            userprofile = ' '.join(pos_list[k].return_value_skills())
+            pos_profile_list.append(userprofile)
 
-# You can change the features you want to use
-pos_profile_list = []
-neg_profile_list = []
-for i in range(len(pos_list)):
-    userprofile = ' '.join(pos_list[i].return_value_all())
-    pos_profile_list.append(userprofile)
+        for k in range(len(neg_list)):
+            userprofile = ' '.join(neg_list[k].return_value_skills())
+            neg_profile_list.append(userprofile)
 
-for i in range(len(neg_list)):
-    userprofile = ' '.join(neg_list[i].return_value_all())
-    neg_profile_list.append(userprofile)
+        pos_label_list = ['pos_profile_'+ str(k) for k in range(len(pos_profile_list))]
+        neg_label_list = ['neg_profile_'+ str(k) for k in range(len(pos_profile_list))]
 
-pos_label_list = ['pos_profile_'+ str(i) for i in range(len(pos_profile_list))]
-neg_label_list = ['neg_profile_'+ str(i) for i in range(len(pos_profile_list))]
+        documents = labeled_data_sentence.LabeledLineSentence(pos_profile_list,pos_label_list,neg_profile_list,neg_label_list)
+        model = Doc2Vec(min_count=1, window=10, size=100, sample=1e-4, negative=5, workers=8)
+        model.build_vocab(documents.to_array())
+        for epoch in range(20):
+            model.train(documents.sentences_perm(), total_examples=model.corpus_count, epochs=model.iter)
+            # model.train(documents.sentences_perm())
+        model.save('/Users/pengyuzhou/Downloads/word_embedding_result/job_title_'+globalparameter.jobtitle_list[i]+'.d2v')
+        model = Doc2Vec.load('/Users/pengyuzhou/Downloads/word_embedding_result/job_title_'+globalparameter.jobtitle_list[i]+'.d2v')
 
-documents = labeled_data_sentence.LabeledLineSentence(pos_profile_list,pos_label_list,neg_profile_list,neg_label_list)
-model = Doc2Vec(min_count=1, window=10, size=100, sample=1e-4, negative=5, workers=8)
-model.build_vocab(documents.to_array())
-for epoch in range(20):
-    model.train(documents.sentences_perm(), total_examples=model.corpus_count, epochs=model.iter)
-    # model.train(documents.sentences_perm())
-model.save('/Users/pengyuzhou/Downloads/word_embedding_result/job_title_'+globalparameter.jobtitle_list[0]+'.d2v')
-model = Doc2Vec.load('/Users/pengyuzhou/Downloads/word_embedding_result/job_title_'+globalparameter.jobtitle_list[0]+'.d2v')
+        test2 = model.most_similar('software')
 
-test2 = model.most_similar('software')
+        train_arrays = numpy.zeros((500,100))
+        train_labels = numpy.zeros(500)
 
-train_arrays = numpy.zeros((500,100))
-train_labels = numpy.zeros(500)
+        for k in range(250):
+            prefix_pos_train = 'pos_profile_'+ str(k)
+            prefix_neg_train = 'neg_profile_'+ str(k)
+            train_arrays[k] = model[prefix_pos_train]
+            train_arrays[250+k] = model[prefix_neg_train]
+            train_labels[k] = 1
+            train_labels[250+k] = 0
 
-for i in range(250):
-    prefix_pos_train = 'pos_profile_'+ str(i)
-    prefix_neg_train = 'neg_profile_'+ str(i)
-    train_arrays[i] = model[prefix_pos_train]
-    train_arrays[250+i] = model[prefix_neg_train]
-    train_labels[i] = 1
-    train_labels[250+i] = 0
+        test_arrays = numpy.zeros((500, 100))
+        test_labels = numpy.zeros(500)
+        for k in range(250):
+            prefix_test_pos = 'pos_profile_' + str(250+k)
+            prefix_test_neg = 'neg_profile_' + str(250+k)
+            test_arrays[k] = model[prefix_test_pos]
+            test_arrays[250 + k] = model[prefix_test_neg]
+            test_labels[k] = 1
+            test_labels[250 + k] = 0
 
-test_arrays = numpy.zeros((500, 100))
-test_labels = numpy.zeros(500)
-for i in range(250):
-    prefix_test_pos = 'pos_profile_' + str(250+i)
-    prefix_test_neg = 'neg_profile_' + str(250+i)
-    test_arrays[i] = model[prefix_test_pos]
-    test_arrays[250 + i] = model[prefix_test_neg]
-    test_labels[i] = 1
-    test_labels[250 + i] = 0
+        alg_logestic_regression.calculate_logistic_regression(train_arrays,train_labels,test_arrays,test_labels,i*8)
+        alg_logestic_regression.calculate_logistic_regression_cv(train_arrays,train_labels,test_arrays,test_labels,i*8)
+        alg_svm.calculate_svm_linear_svc(train_arrays,train_labels,test_arrays,test_labels,i*8)
+        alg_svm.calculate_svm_nusvc(train_arrays,train_labels,test_arrays,test_labels,i*8)
+        alg_svm.calculate_svm_svc(train_arrays,train_labels,test_arrays,test_labels,i*8)
+        alg_bayes.calculate_bayes(train_arrays,train_labels,test_arrays,test_labels,i*8)
+        alg_decision_tree.calculate_decision_tree(train_arrays,train_labels,test_arrays,test_labels,i*8)
+        alg_ramdom_forest.calculate_random_forest(train_arrays,train_labels,test_arrays,test_labels,i*8)
 
-classifier = LogisticRegression()
-classifier.fit(train_arrays, train_labels)
-prediction = classifier.predict(test_arrays)
-accuracy_score_p = accuracy_score(test_labels,prediction)
-accuracy_score = classifier.score(test_arrays, test_labels)
-precision_score = precision_score(test_labels,prediction)
-recall_score = recall_score(test_labels,prediction)
+        print('end this iteration')
+    print('Switch to next job title.')
 
-print('accuracy_score_p is : '+str(accuracy_score_p))
-print('accuracy_score is : '+ str(accuracy_score_p))
-print('precision acore is :' + str(precision_score))
-print('recall score is :' + str(recall_score))
+for i in range(48):
+    globalparameter.alg_precision[i] = (globalparameter.alg_precision[i]) / 5
+    globalparameter.alg_recall[i] = (globalparameter.alg_recall[i]) / 5
+    globalparameter.alg_accuracy[i] = (globalparameter.alg_accuracy[i]) / 5
+    globalparameter.alg_f1_score[i] = (globalparameter.alg_f1_score[i]) / 5
+
+print('Precision value list: {}'.format(globalparameter.alg_precision))
+print('Recall value list: {}'.format(globalparameter.alg_recall))
+print('Accuracy value list: {}'.format(globalparameter.alg_accuracy))
+result_list = pd.DataFrame({'precision': globalparameter.alg_precision, 'recall': globalparameter.alg_recall,
+                            'accuracy': globalparameter.alg_accuracy, 'f1_score': globalparameter.alg_f1_score})
+result_list.to_csv('/Users/pengyuzhou/Downloads/word_embedding_result/all_result_list.csv')
+
+# classifier = LogisticRegression()
+# classifier.fit(train_arrays, train_labels)
+# prediction = classifier.predict(test_arrays)
+# accuracy_score_p = accuracy_score(test_labels,prediction)
+# accuracy_score = classifier.score(test_arrays, test_labels)
+# precision_score = precision_score(test_labels,prediction)
+# recall_score = recall_score(test_labels,prediction)
+# 
+# print('accuracy_score_p is : '+str(accuracy_score_p))
+# print('accuracy_score is : '+ str(accuracy_score_p))
+# print('precision acore is :' + str(precision_score))
+# print('recall score is :' + str(recall_score))
 
 
 print(1)
